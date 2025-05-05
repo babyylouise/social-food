8987b7343368b946ff50ba17e2785e944e1718a0
Ellen Louise <ellendsantos1@gmail.com>
2025-05-05 18:16:51 -0300

    Primeiro commit do meu projeto

---
+++ mean-backend/routes/projectApi.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const {
+    getProjects,
+    createProject,
+    getProject,
+    updateProject,
+    deleteProject
+} = require('../controller/projectController')
+ 
+ 
+const router = express.Router()
+ 
+// get list of projects
+router.get('/', getProjects)
+ 
+// create a new project
+router.post('/', createProject)
+ 
+// get specific project
+router.get('/:id', getProject)
+ 
+// update a project
+router.patch('/:id', updateProject)
+ 
+// delete a project
+router.delete('/:id', deleteProject)
+ 
+ 
+module.exports = router
